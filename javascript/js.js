69f01fce5bee685f3347b8091e0dd257f3885108
zomadk <36054397+zomadk@users.noreply.github.com>
2025-03-07 12:24:33 -0500

    Add files via upload

--- javascript/js.js
+++ javascript/js.js
@@ -1,33 +1,19 @@
-//alert("Pages Linked!")
-//$("#container")
-//$("body")
+//alert("Hello World!")
 
-    /*if (this is true){
-        do this
-    }
-    else{
-        do this
-    }*/
+$(".menu-area").click(function() {
+  //$(".menu-area").addClass("active")
 
-
-$(".menu-area").click(function(){
-    //$(".menu-area").addClass("active")
-
-   /*if($(".menu-area").hasClass("active")){
+  if($(".menu-area").hasClass("active")){
     $(".menu-area").removeClass("active")
-   }
-   else{
+  }
+  else{
     $(".menu-area").addClass("active")
-   }*/
-    $(".menu-area").toggleClass("active")
-    console.log("Menu clicked")
+  }
 })
 
-$("#block-1").click(function(){
-    $("#block-5").toggleClass("clicked")
-    $("#block-2").css("background-color","yellow")
-})
-
-
-
+$(".menu-area").hover(function(){
+    $(".grid-sections").css("background-color", "yellow");
+    }, function(){
+    $(".grid-sections").css("background-color", "pink");
+  });
 
